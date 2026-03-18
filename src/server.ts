ca47ca26b0f2a058e2e7348b2ad09a0094ad1c89
hrhabib07 <mdhabibur.hr7@gmail.com>
2026-03-18 13:38:12 +0600

    serve updated

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import "dotenv/config";
 import app from "./app";
 import { connectDB } from "./config/db";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    app.listen(PORT, HOST, () => {
+      console.log(`Server listening on http://${HOST}:${PORT}`);
     });
   })
   .catch((err) => {
